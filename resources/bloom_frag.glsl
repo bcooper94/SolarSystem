b78d7c04ca14277bb899bced06b984e2ec03429f
bcooper94 <bcoopercs@live.com>
2016-03-16 13:00:52 -0700

    Working low res render to texture

--- resources/bloom_frag.glsl
+++ resources/bloom_frag.glsl
@@ -7,13 +7,14 @@ in vec2 texCoord;
 out vec4 color;
 
 vec2 getTexCoords() {
-   return gl_FragCoord.xy * 0.5 + vec2(0.5, 0.5);
+   return vec2(gl_FragCoord.x / 1280, gl_FragCoord.y / 960);
 }
 
 void main() {
    // color = vec4(1, 0, 1, 1);
-   // vec2 texCoord = getTexCoords();
+   vec2 texCoord = getTexCoords();
    color = texture(renderedTexture, texCoord);
+   // color = vec4(texCoord, 0, 1);
    // color = vec4(0, 1, 0, 1);
    // float grayScale = (color.r + color.g + color.b) / 3;
    // color = vec4(vec3(grayScale), 1);
--- resources/bloom_vert.glsl
+++ resources/bloom_vert.glsl
@@ -3,9 +3,9 @@
 layout(location = 0) in vec4 vertPos;
 layout(location = 1) in vec2 vertTex;
 
-out vec2 texCoord;
+// out vec2 texCoord;
 
 void main() {
    gl_Position = vertPos;
-   texCoord = vertTex;
+   // texCoord = vertTex;
 }
